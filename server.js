759687a665a4564c77fe212c122a93912564c40e
Andrew Ogilvy <ogilvyandrew@gmail.com>
2021-07-01 15:06:17 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+require('dotenv').config();
+require('./config/database');
+
+const app = express();
+// returns an express app
+
+//app.use we are mounting middleware
+
+app.use(logger('dev'));
+app.use(express.json());
+//must be configured to serve from the build folder
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// get the server ti listen
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app is running on port ${port}`)
+});
+
+// catch all route
+// catch any get requests that doesnt match one of our api routes
+// put API routes here, before the "catch all route"
+
+
+//The following "catch all" route (note the *) is nessecary to rerturn the index.html on all non-AJAX requests
+// /* matches anything
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
